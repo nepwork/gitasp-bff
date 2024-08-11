e6401211c5f375b751c90f130b27d2b7d38020c0
Risav Karna <git@risav.dev>
2024-08-11 13:57:20 +0545

    fix(cursed): add table and grid both

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import svelte from "@astrojs/svelte";
-const DEV_PORT = 2121;
 
+const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
   base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
   output: 'hybrid',
-  experimental: {
-    assets: true
-  },
   /* Like Vercel, Netlify,… Mimicking for dev. server */
   // trailingSlash: 'always',
   vite: {
